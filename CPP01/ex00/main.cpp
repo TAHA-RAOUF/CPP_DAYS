b9395da6bf7d4dd9b64c2b3ba09b884d863eea9b
Mohamed Taha Raouf <moraouf@c1r1p8.1337.ma>
2025-12-22 18:22:32 +0100

    add the comments at the main

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: moraouf <moraouf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 19:31:00 by moraouf           #+#    #+#             */
-/*   Updated: 2025/12/22 17:39:16 by moraouf          ###   ########.fr       */
+/*   Updated: 2025/12/22 18:10:33 by moraouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int main()
 {
+    std::cout << "----Stack Zombie----" << std::endl;
     ramdomChump("Black_wolf");
+    std::cout << "----Heap Zombie----" << std::endl;
     Zombie* zombie = newZombie("Taha");
     zombie->announce();
     delete zombie;
